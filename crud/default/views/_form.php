c2699f40678c83f7652aa1ca43f9e4f68ace97ee
Byron Adams <byron@webtools.co.nz>
2015-04-02 11:47:32 +1300

    Fixed form template formatting

--- crud/default/views/_form.php
+++ crud/default/views/_form.php
@@ -32,86 +32,61 @@ use \dmstr\bootstrap\Tabs;
 <div class="<?= \yii\helpers\Inflector::camel2id(StringHelper::basename($generator->modelClass), '-', true) ?>-form">
 
     <?= "<?php " ?>$form = ActiveForm::begin([
-                        'id'     => '<?= $model->formName() ?>',
-                        'layout' => '<?= $generator->formLayout ?>',
-                        'enableClientValidation' => false,
-                    ]
-                );
-    ?>
+        'id' => '<?= $model->formName() ?>',
+        'layout' => '<?= $generator->formLayout ?>',
+        'enableClientValidation' => false,
+    ]); ?>
 
-    <div class="">
-        <?= "<?php " ?>echo $form->errorSummary($model); ?>
-        <?php echo "<?php \$this->beginBlock('main'); ?>\n"; ?>
+    <?= "<?php " ?>echo $form->errorSummary($model); ?>
+    <?php echo "<?php \$this->beginBlock('main'); ?>\n"; ?>
+    <?php foreach ($safeAttributes as $attribute) {
+        if (!isset($generator->getTableSchema()->columns[$attribute])) {
+            continue;
+        }
 
-        <p>
-            <?php foreach ($safeAttributes as $attribute) {
+        $column   = $generator->getTableSchema()->columns[$attribute];
+        $prepend = $generator->prependActiveField($column, $model);
+        $field = $generator->activeField($column, $model);
+        $append = $generator->appendActiveField($column, $model);
 
-                if (!isset($generator->getTableSchema()->columns[$attribute])) {
-                    continue;
-                }
+        if ($prepend) {
+            echo "\n    <?php " . $prepend . " ?>";
+        }
+        if ($field) {
+            echo "\n    <?= " . $field . " ?>";
+        }
+        if ($append) {
+            echo "\n    <?php " . $append . " ?>";
+        }
+    } ?>
 
-                $column   = $generator->getTableSchema()->columns[$attribute];
+    <?php echo "<?php \$this->endBlock(); ?>"; ?>
 
-                $prepend = $generator->prependActiveField($column, $model);
-                $field = $generator->activeField($column, $model);
-                $append = $generator->appendActiveField($column, $model);
+    <?php $label = $generator->modelClass.'::label()';?>
 
-                if ($prepend) {
-                    echo "\n            <?php " . $prepend . " ?>";
-                }
-                if ($field) {
-                    echo "\n            <?= " . $field . " ?>";
-                }
-                if ($append) {
-                    echo "\n            <?php " . $append . " ?>";
-                }
-            } ?>
+    <?= "<?= Tabs::widget([
+        'encodeLabels' => false,
+        'items' => [
+            [
+                'label'   => $label,
+                'content' => \$this->blocks['main'],
+                'active'  => true,
+            ],
+        ]
+    ]);?>"; ?>
 
-        </p>
-        <?php echo "<?php \$this->endBlock(); ?>"; ?>
+    <hr/>
 
-        <?php
-        $label = $generator->modelClass.'::label()';
+    <?= "<?= " ?>Html::submitButton('<span class="glyphicon glyphicon-check"></span> ' . ($model->isNewRecord ? <?= $generator->generateString('Create') ?> : <?= $generator->generateString('Save') ?>), [
+        'id'    => 'save-' . $model->formName(),
+        'class' => 'btn btn-primary'
+    ]); ?>
 
-        $items = <<<EOS
-[
-    'label'   => $label,
-    'content' => \$this->blocks['main'],
-    'active'  => true,
-],
-EOS;
-        ?>
-
-        <?=
-        "<?=
-    Tabs::widget(
-                 [
-                   'encodeLabels' => false,
-                     'items' => [ $items ]
-                 ]
-    );
-    ?>";
-        ?>
-
-        <hr/>
-
-        <?= "<?= " ?>Html::submitButton(
-                '<span class="glyphicon glyphicon-check"></span> ' . ($model->isNewRecord
-                            ? <?= $generator->generateString('Create') ?> : <?= $generator->generateString('Save') ?>),
-                [
-                    'id'    => 'save-' . $model->formName(),
-                    'class' => 'btn btn-primary'
-                ]
-            );
-        ?>
-
-        <p class="pull-right">
-            <?= "<?= " ?>Html::a(<?= $generator->generateString('Cancel') ?>, \yii\helpers\Url::previous(), ['class' => 'btn btn-default']) ?>
-        </p>
+    <p class="pull-right">
+        <?= "<?= " ?>Html::a(<?= $generator->generateString('Cancel') ?>, \yii\helpers\Url::previous(), ['class' => 'btn btn-default']) ?>
+    </p>
 
 
-        <?= "<?php " ?>ActiveForm::end(); ?>
-
-    </div>
+    <?= "<?php " ?>ActiveForm::end(); ?>
 
 </div>

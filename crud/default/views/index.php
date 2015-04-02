86de0b04e70069d9670c4a9214e92742229a2509
Byron Adams <byron@webtools.co.nz>
2015-04-02 14:39:54 +1300

    Cleaned up index template

--- crud/default/views/index.php
+++ crud/default/views/index.php
@@ -31,114 +31,55 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass), '-', true) ?>-index">
 
     <h1><?= '<?= $searchModel->label(2) ?>' ?></h1>
-    <?=
-    "<?php " . ($generator->indexWidgetType === 'grid' ? "// " : "") ?>
-    echo $this->render('_search', ['model' =>$searchModel]);
-    ?>
 
     <div class="clearfix">
         <p class="pull-left">
             <?= "<?= " ?>Html::a('<span class="glyphicon glyphicon-plus"></span> ' . <?= $generator->generateString('New') ?> . ' <?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?>', ['create'], ['class' => 'btn btn-success']) ?>
         </p>
-
-        <div class="pull-right">
-
-
-            <?php
-            $items = [];
-            $model = new $generator->modelClass;
-            ?>
-            <?php foreach ($generator->getModelRelations($model) AS $relation): ?>
-                <?php
-                // relation dropdown links
-                $iconType = ($relation->multiple) ? 'arrow-right' : 'arrow-left';
-                if ($generator->isPivotRelation($relation)) {
-                    $iconType = 'random';
-                }
-                $controller = $generator->pathPrefix . Inflector::camel2id(
-                        StringHelper::basename($relation->modelClass),
-                        '-',
-                        true
-                    );
-                $route = $generator->createRelationRoute($relation,'index');
-                $label      = Inflector::titleize(StringHelper::basename($relation->modelClass), '-', true);
-                $items[] = [
-                    'label' => '<i class="glyphicon glyphicon-' . $iconType . '"> ' . $label . '</i>',
-                    'url'   => [$route]
-                ]
-                ?>
-            <?php endforeach; ?>
-
-            <?php if (!empty($items)): ?>
-                <?= "<?= \n" ?>
-                \yii\bootstrap\ButtonDropdown::widget(
-                [
-                'id'       => 'giiant-relations',
-                'encodeLabel' => false,
-                'label'    => '<span class="glyphicon glyphicon-paperclip"></span> ' . <?= $generator->generateString('Relations') ?>,
-                'dropdown' => [
-                'options'      => [
-                'class' => 'dropdown-menu-right'
-                ],
-                'encodeLabels' => false,
-                'items'        => <?= \yii\helpers\VarDumper::export($items) ?>
-                ],
-                ]
-                );
-                <?= "?>" ?>
-            <?php endif; ?>
-        </div>
     </div>
 
     <?php if ($generator->indexWidgetType === 'grid'): ?>
 
-        <div class="table-responsive">
-            <?= "<?= " ?>GridView::widget([
+    <div class="table-responsive">
+        <?= "<?= " ?>GridView::widget([
             'layout' => '{summary}{pager}{items}{pager}',
             'dataProvider' => $dataProvider,
-            'pager'        => [
-            'class'          => yii\widgets\LinkPager::className(),
-            'firstPageLabel' => <?= $generator->generateString('First') ?>,
-            'lastPageLabel'  => <?= $generator->generateString('Last') ?>
+            'pager' => [
+                'class' => yii\widgets\LinkPager::className(),
+                'firstPageLabel' => <?= $generator->generateString('First') ?>,
+                'lastPageLabel' => <?= $generator->generateString('Last') ?>,
             ],
             'filterModel' => $searchModel,
-            'columns' => [
-
-            <?php
-            $actionButtonColumn = <<<PHP
-[
-    'class' => '{$generator->actionButtonClass}',
-    'urlCreator' => function(\$action, \$model, \$key, \$index) {
-        // using the column name as key, not mapping to 'id' like the standard generator
-        \$params = is_array(\$key) ? \$key : [\$model->primaryKey()[0] => (string) \$key];
-        \$params[0] = \Yii::\$app->controller->id ? \Yii::\$app->controller->id . '/' . \$action : \$action;
-        return Url::toRoute(\$params);
-    },
-    'contentOptions' => ['nowrap'=>'nowrap']
-],
-PHP;
-
-
-
+            'columns' => [<?php echo "\n";
             $count = 0;
-            echo "\n"; // code-formatting
-
+            $model = new $generator->modelClass;
             foreach ($generator->getTableSchema()->columns as $column) {
                 $format = trim($generator->columnFormat($column,$model));
                 if ($format == false) continue;
                 if (++$count < 8) {
-                    echo "\t\t\t{$format},\n";
+                    echo "                {$format},\n";
                 } else {
-                    echo "\t\t\t/*{$format}*/\n";
+                    echo "                /*{$format}*/\n";
                 }
             }
 
-            // action buttons last
-            echo $actionButtonColumn;
+            echo <<<PHP
+                [
+                    'class' => '{$generator->actionButtonClass}',
+                    'urlCreator' => function(\$action, \$model, \$key, \$index) {
+                        // using the column name as key, not mapping to 'id' like the standard generator
+                        \$params = is_array(\$key) ? \$key : [\$model->primaryKey()[0] => (string) \$key];
+                        \$params[0] = \Yii::\$app->controller->id ? \Yii::\$app->controller->id . '/' . \$action : \$action;
+                        return Url::toRoute(\$params);
+                    },
+                    'contentOptions' => ['nowrap'=>'nowrap']
+                ],
+PHP;
+            echo "\n";
             ?>
             ],
-            ]); ?>
-        </div>
+        ]); ?>
+    </div>
 
     <?php else: ?>
 

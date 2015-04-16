ed4ddb2733c4ebc75fe059b68d35d0fb7d02db5b
Byron Adams <byron@webtools.co.nz>
2015-04-16 19:18:49 +1200

    removing search method from model, and fixing curly brace on class

--- crud/default/search.php
+++ crud/default/search.php
@@ -36,7 +36,7 @@ use <?= ltrim($generator->modelClass, '\\') . (isset($modelAlias) ? " as $modelA
 /**
 * <?= $searchModelClass ?> represents the model behind the search form about `<?= $generator->modelClass ?>`.
 */
-class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $modelClass ?>
+class <?= $searchModelClass ?> extends <?= (isset($modelAlias) ? $modelAlias : $modelClass).PHP_EOL ?>
 {
     /**
      * @inheritdoc
@@ -55,35 +55,5 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
     {
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
-    }
-
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = <?= isset($modelAlias) ? $modelAlias : $modelClass ?>::find();
-
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
-        <?= implode("\n        ", $searchConditions) ?>
-
-        $query->orderBy('<?=$primaryKey?> DESC');
-
-        return $dataProvider;
     }
 }

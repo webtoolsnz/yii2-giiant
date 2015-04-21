be4c2f73745dd598e3054ee1c96dd491ca7ef970
Byron Adams <byron@webtools.co.nz>
2015-04-21 16:26:08 +1200

    Altering search conditions generators

--- model/Generator.php
+++ model/Generator.php
@@ -252,12 +252,12 @@ class Generator extends \yii\gii\generators\model\Generator
                 case Schema::TYPE_DOUBLE:
                 case Schema::TYPE_DECIMAL:
                 case Schema::TYPE_MONEY:
+                    $hashConditions[] = "'{$column}' => \$this->{$column},";
+                    break;
                 case Schema::TYPE_DATE:
                 case Schema::TYPE_TIME:
                 case Schema::TYPE_DATETIME:
                 case Schema::TYPE_TIMESTAMP:
-                    $hashConditions[] = "'{$column}' => \$this->{$column},";
-                    break;
                 default:
                     $likeConditions[] = "->andFilterWhere(['like', '{$column}', \$this->{$column}])";
                     break;

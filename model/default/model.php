2737cfdb7fe3bc20b70ad3244f5803ae61e8a1b5
Byron Adams <byron@webtools.co.nz>
2015-05-28 10:58:02 +1200

    Removing unessicary return, was preventing manual filters being applied

--- model/default/model.php
+++ model/default/model.php
@@ -110,11 +110,7 @@ class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . 
 <?php endif ?>
         ]);
 
-        if (!$this->load($params, $formName)) {
-            // uncomment the following line if you do not want to any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
+        $this->load($params, $formName);
 
 <?= implode("\n ", $searchConditions) ?>
 

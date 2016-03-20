8bf98a59c2cea6d315c7ce44f7682478110dd006
Byron Adams <byron.adams54@gmail.com>
2016-03-21 12:56:13 +1300

    Adding support for Gii 2.0.5

--- model/form.php
+++ model/form.php
@@ -5,6 +5,8 @@
  * @var yii\gii\generators\form\Generator $generator
  */
 
+use yii\gii\generators\model\Generator;
+
 echo $form->field($generator, 'tableName');
 echo $form->field($generator, 'tablePrefix');
 echo $form->field($generator, 'modelClass');
@@ -13,7 +15,11 @@ echo $form->field($generator, 'ignoreTables');
 echo $form->field($generator, 'ns');
 echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'db');
-echo $form->field($generator, 'generateRelations')->checkbox();
+echo $form->field($generator, 'generateRelations')->dropDownList([
+    Generator::RELATIONS_NONE => 'No relations',
+    Generator::RELATIONS_ALL => 'All relations',
+    Generator::RELATIONS_ALL_INVERSE => 'All relations with inverse',
+]);
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
 echo $form->field($generator, 'generateModelClass')->checkbox();
 echo $form->field($generator, 'enableI18N')->checkbox();
